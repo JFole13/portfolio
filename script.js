d7d224d65bdb5af5acd47e5ecf87e38e670f4b95
Jesse Foley <jessefoley10@yahoo.com>
2024-03-11 19:18:57 -0500

    start modal content design

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-var modal = document.getElementById('projectModal');
+let projectModal = document.querySelector('.project-modal');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-button")[0];
+let modalCloseButton = document.querySelector('.modal-close-btn');
 
-// When the user clicks on the project, open the modal 
 document.querySelectorAll('.project').forEach(item => {
   item.addEventListener('click', function() {
-    modal.style.display = "block";
-    // You can also dynamically change the modal content here based on the project clicked
+    projectModal.style.display = "flex";
   });
 });
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+modalCloseButton.onclick = function() {
+  projectModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == projectModal) {
+//     projectModal.style.display = "none";
+//   }
+// }
 
-// When the user clicks on <span> (x), close the modal with animation
-span.onclick = function() {
-    modal.querySelector('.modal-content').classList.add('close');
-    setTimeout(() => {
-      modal.style.display = "none";
-      modal.querySelector('.modal-content').classList.remove('close'); // Remove the class so it can animate again when reopened
-    }, 100); // Should match the animation-duration
-  }
+// modalCloseButton.onclick = function() {
+//   projectModal.querySelector('.modal-content').classList.add('close');
+//     setTimeout(() => {
+//       projectModal.style.display = "none";
+//       projectModal.querySelector('.modal-content').classList.remove('close'); // Remove the class so it can animate again when reopened
+//     }, 100);
+//   }
   
-  // Modify the existing window.onclick event as well
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.querySelector('.modal-content').classList.add('close');
-      setTimeout(() => {
-        modal.style.display = "none";
-        modal.querySelector('.modal-content').classList.remove('close');
-      }, 100); // Match the animation duration
-    }
-  }
+//   window.onclick = function(event) {
+//     if (event.target == projectModal) {
+//       projectModal.querySelector('.modal-content').classList.add('close');
+//       setTimeout(() => {
+//         projectModal.style.display = "none";
+//         projectModal.querySelector('.modal-content').classList.remove('close');
+//       }, 100); // Match the animation duration
+//     }
+//   }
