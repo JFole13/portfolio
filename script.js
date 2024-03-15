c085c43c4657e6afddfa2fa8bd941b11ced885b5
Jesse Foley <jessefoley10@yahoo.com>
2024-03-15 01:45:28 -0500

    finish the rest of the modals for the project previews

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ const saltyPreview = document.querySelector('.salty-project');
 const weddingPreview = document.querySelector('.wedding-project');
 const aliggettdesignsPreview = document.querySelector('.aliggettdesigns-project');
 const kylePreview = document.querySelector('.kyle-project');
-
+const hindsitePreview = document.querySelector('.hindsite-project');
+const blackjackPreview = document.querySelector('.blackjack-project');
 
 const generateSaltyPreviewContent = () => {
   modalTitle.innerHTML = 'SALTY League';
@@ -83,7 +84,7 @@ const generateALiggettDesignsPreviewContent = () => {
     <p>This is a current <strong>work in progress</strong>, but we have a live beta version of the online clothing shop. </p>
     <p>Client has access to a simple CMS I created, where she can update almost everything on the screen to her liking.
         Products can be added, updated, or deleted as well. </p>
-    <p>Products listed can be purchased through Venmo as of right now, while we are working and testing on adding other types 
+    <p>Listed products can be purchased through Venmo as of right now, while we are working and testing on adding other types 
         of payment support.</p>`;
   modalImg.src = 'images/a-liggett-designs-modal-preview.svg';
   modalStackIconsContainer.innerHTML = '';
@@ -125,10 +126,63 @@ const generateKylePreviewContent = () => {
   modalLink.target = '_blank';
 }
 
+const generateHindsitePreviewContent = () => {
+  modalTitle.innerHTML = 'hindsite';
+  modalDescriptionContainer.innerHTML = `
+    <p><strong>hindsite</strong> is a trend tracker for all 32 teams in the NHL. After every single matchup, <strong>hindsite</strong>
+        will sort the data in the proper categories, like adding a win in the "Away Win" and "Away Over 6" Category if the Colorado
+        Avalanche beat the Calgary Flames 5-3 up in Canada.</p>
+    <p>Unfortunately the project is defunct for now, as some as the necessary packages aren't supported anymore. I loved this 
+        one though, since it was one of my biggest and was able to practice using multiple technologies in one project.</p>`;
+  modalImg.src = 'images/hindsite-modal-preview.svg';
+  modalStackIconsContainer.innerHTML = '';
+
+  modalStackIconsContainer.append(createModalStackIcon('images/python-logo.png', 'Python Logo'));
+  modalStackIconsContainer.append(createModalStackIcon('images/javascript-logo.png', 'JavaScript Logo'));
+  modalStackIconsContainer.append(createModalStackIcon('images/node-logo.png', 'Node JS Logo'));
+  modalStackIconsContainer.append(createModalStackIcon('images/mysql-logo.png', 'MySQL Logo'));
+
+  const gitHubIcon = createModalStackIcon('images/github-logo.png', 'GitHub Logo');
+  gitHubIcon.classList.add('modal-stack-icon');
+  const gitHubIconLink = document.createElement('a');
+  gitHubIconLink.href = 'https://github.com/JFole13/hindsite';
+  gitHubIconLink.target = '_blank';
+  gitHubIconLink.append(gitHubIcon);
+  modalStackIconsContainer.append(gitHubIconLink);
+  modalLink.href = '#';
+  modalLink.target = '_self';
+}
+
+const generateBlackJackPreviewContent = () => {
+  modalTitle.innerHTML = 'Blackjack';
+  modalDescriptionContainer.innerHTML = `
+    <p>This is a game of normal <strong>blackjack</strong>, with the options to double down and pay insurance. It's a static site,
+        with game logic being handled with vanilla javascript. Animations are made with plain CSS.</p>
+    <p>This is my very first project I made by myself when I first started getting into web development and it's still one of my
+        favorites. It's a classic game that still holds up as a solid project. This is one that I look at and am still proud of
+        over the years of creating things.</p>`;
+  modalImg.src = 'images/blackjack-modal-preview.svg';
+  modalStackIconsContainer.innerHTML = '';
+
+  modalStackIconsContainer.append(createModalStackIcon('images/javascript-logo.png', 'JavaScript Logo'));
+
+  const gitHubIcon = createModalStackIcon('images/github-logo.png', 'GitHub Logo');
+  gitHubIcon.classList.add('modal-stack-icon');
+  const gitHubIconLink = document.createElement('a');
+  gitHubIconLink.href = 'https://github.com/JFole13/blackjack';
+  gitHubIconLink.target = '_blank';
+  gitHubIconLink.append(gitHubIcon);
+  modalStackIconsContainer.append(gitHubIconLink);
+  modalLink.href = 'https://jessefoley.com/blackjack/index.html';
+  modalLink.target = '_blank';
+}
+
 saltyPreview.addEventListener('click', generateSaltyPreviewContent);
 weddingPreview.addEventListener('click', generateWeddingPreviewContent);
 aliggettdesignsPreview.addEventListener('click', generateALiggettDesignsPreviewContent);
 kylePreview.addEventListener('click', generateKylePreviewContent);
+hindsitePreview.addEventListener('click', generateHindsitePreviewContent);
+blackjackPreview.addEventListener('click', generateBlackJackPreviewContent);
 
 document.querySelectorAll('.project').forEach(item => {
   item.addEventListener('click', function() {
