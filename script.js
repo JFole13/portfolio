2aac39d2ab2991c94b007e461e93d11f8f34f3a0
Jesse Foley <jessefoley10@yahoo.com>
2024-03-18 19:14:53 -0500

    Final responsiveness check

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ const blackjackPreview = document.querySelector('.blackjack-project');
 const generateSaltyPreviewContent = () => {
   modalTitle.innerHTML = 'SALTY League';
   modalDescriptionContainer.innerHTML = `
-    <p>My most recent and favorite project as of right now, <strong>SALTY League</strong> is a custom scoring system for me 
+    <p class="modal-description-text">My most recent and favorite project as of right now, <strong>SALTY League</strong> is a custom scoring system for me 
         and my buddies fantasy football league.</p>
-    <p>We wanted a way to see the victor over a span of multiple years instead of one, and that's when we 
+    <p class="modal-description-text">We wanted a way to see the victor over a span of multiple years instead of one, and that's when we 
         came up with the points system that SALTY uses.</p>
-    <p>Using the Sleeper API, SALTY automatically takes each week's matchup outcomes and calculates the 
+    <p class="modal-description-text">Using the Sleeper API, SALTY automatically takes each week's matchup outcomes and calculates the 
         player's SALTY score, updates all rankings and displays corresponding activity messages.</p>`;
   modalImg.src = 'images/salty-modal-preview.svg';
   modalStackIconsContainer.innerHTML = '';
@@ -56,9 +56,9 @@ const generateSaltyPreviewContent = () => {
 const generateWeddingPreviewContent = () => {
   modalTitle.innerHTML = 'Wedding Website';
   modalDescriptionContainer.innerHTML = `
-    <p><strong>I'm getting married this year!</strong></p>
-    <p>Every wedding needs a good wedding website so I wanted to make my own.</p>
-    <p>It has a details section accompanied by the venues location through the Google Maps API. 
+    <p class="modal-description-text"><strong>I'm getting married this year!</strong></p>
+    <p class="modal-description-text">Every wedding needs a good wedding website so I wanted to make my own.</p>
+    <p class="modal-description-text">It has a details section accompanied by the venues location through the Google Maps API. 
         The RSVP and message board inputs are handled through MongoDB. </p>`;
   modalImg.src = 'images/wedding-modal-preview.svg';
   modalStackIconsContainer.innerHTML = '';
@@ -81,10 +81,10 @@ const generateWeddingPreviewContent = () => {
 const generateALiggettDesignsPreviewContent = () => {
   modalTitle.innerHTML = 'A Liggett Designs';
   modalDescriptionContainer.innerHTML = `
-    <p>This is a current <strong>work in progress</strong>, but we have a live beta version of the online clothing shop. </p>
-    <p>Client has access to a simple CMS I created, where she can update almost everything on the screen to her liking.
+    <p class="modal-description-text">This is a current <strong>work in progress</strong>, but we have a live beta version of the online clothing shop. </p>
+    <p class="modal-description-text">Client has access to a simple CMS I created, where she can update almost everything on the screen to her liking.
         Products can be added, updated, or deleted as well. </p>
-    <p>Listed products can be purchased through Venmo as of right now, while we are working and testing on adding other types 
+    <p class="modal-description-text">Listed products can be purchased through Venmo as of right now, while we are working and testing on adding other types 
         of payment support.</p>`;
   modalImg.src = 'images/a-liggett-designs-modal-preview.svg';
   modalStackIconsContainer.innerHTML = '';
@@ -108,8 +108,8 @@ const generateALiggettDesignsPreviewContent = () => {
 const generateKylePreviewContent = () => {
   modalTitle.innerHTML = 'Kyle Thompson';
   modalDescriptionContainer.innerHTML = `
-    <p>Static site <strong>portfolio</strong> for my colleague.</p>
-    <p>Display's projects, work experience, and written works.</p>`;
+    <p class="modal-description-text">Static site <strong>portfolio</strong> for my colleague.</p>
+    <p class="modal-description-text">Display's projects, work experience, and written works.</p>`;
   modalImg.src = 'images/kyle-portfolio-modal-preview.svg';
   modalStackIconsContainer.innerHTML = '';
 
@@ -129,10 +129,10 @@ const generateKylePreviewContent = () => {
 const generateHindsitePreviewContent = () => {
   modalTitle.innerHTML = 'hindsite';
   modalDescriptionContainer.innerHTML = `
-    <p><strong>hindsite</strong> is a trend tracker for all 32 teams in the NHL. After every single matchup, <strong>hindsite</strong>
+    <p class="modal-description-text"><strong>hindsite</strong> is a trend tracker for all 32 teams in the NHL. After every single matchup, <strong>hindsite</strong>
         will sort the data in the proper categories, like adding a win in the "Away Win" and "Away Over 6" Category if the Colorado
         Avalanche beat the Calgary Flames 5-3 up in Canada.</p>
-    <p>Unfortunately the project is defunct for now, as some as the necessary packages aren't supported anymore. I loved this 
+    <p class="modal-description-text">Unfortunately the project is defunct for now, as some as the necessary packages aren't supported anymore. I loved this 
         one though, since it was one of my biggest and was able to practice using multiple technologies in one project.</p>`;
   modalImg.src = 'images/hindsite-modal-preview.svg';
   modalStackIconsContainer.innerHTML = '';
@@ -156,9 +156,9 @@ const generateHindsitePreviewContent = () => {
 const generateBlackJackPreviewContent = () => {
   modalTitle.innerHTML = 'Blackjack';
   modalDescriptionContainer.innerHTML = `
-    <p>This is a game of normal <strong>blackjack</strong>, with the options to double down and pay insurance. It's a static site,
+    <p class="modal-description-text">This is a game of normal <strong>blackjack</strong>, with the options to double down and pay insurance. It's a static site,
         with game logic being handled with vanilla javascript. Animations are made with plain CSS.</p>
-    <p>This is my very first project I made by myself when I first started getting into web development and it's still one of my
+    <p class="modal-description-text">This is my very first project I made by myself when I first started getting into web development and it's still one of my
         favorites. It's a classic game that still holds up as a solid project. This is one that I look at and am still proud of
         over the years of creating things.</p>`;
   modalImg.src = 'images/blackjack-modal-preview.svg';
